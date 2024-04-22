34ba6a35d579e0a342f79a1fc38cfb2c56b416dd
mrchysen <mrchysen@mail.ru>
2024-04-22 21:47:27 +0700

    Добавьте файлы проекта.

---
+++ CombiAlg-3.1/Program.cs
@@ -0,0 +1,52 @@
+﻿using Logic;
+
+var g = GraphReader.ReadGraph("graph3.txt");
+
+#region Компонент связности
+bool[] used = new bool[g.Count];
+int i = 0;
+while (used.Any(el => !el))
+{
+    var v = -1;
+
+    for (int j = 0; j < g.Count; j++)
+    {
+        if (!used[j])
+        {
+            v = j;
+            break;
+        }
+    }
+
+    i++;
+
+    GraphAlgorithms.DFS(g, v, used);
+}
+Console.WriteLine("Компонент связности: " + i);
+#endregion
+
+#region Достижимость
+var u = 0;
+used = GraphAlgorithms.BFS(g, u);
+Console.WriteLine($"Из вершины {u+1} достижимы: ");
+for (int k = 0; k < g.Count; k++)
+{
+    if (used[k] && k != u)
+        Console.WriteLine(k+1);
+}
+#endregion
+
+#region Отыскание остовного дерева
+u = 0;
+(used, var Y) = GraphAlgorithms.BFSModified(g, u);
+
+for (int k = 0; k < Y.Length; k++)
+{
+    if(k != u && Y[k] != -1)
+    {
+        Console.WriteLine($"Из {Y[k]} в {k}");
+    }
+}
+#endregion
+
+
---
+++ Logic/GraphAlgorithms.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic;
+
+public static class GraphAlgorithms
+{
+    public static bool[] DFS(List<List<int>> g, int s, bool[]? used = null)
+    {
+        if(used == null) 
+            used = new bool[g.Count];
+
+        DFSrecursive(g, s, used);
+
+        return used;
+    }
+
+    private static void DFSrecursive(List<List<int>> g, int s, bool[] used)
+    {
+        Console.WriteLine(s + 1);
+        
+        used[s] = true;
+
+        for (int i = 0; i < g[s].Count; i++)
+        {
+            if (!used[g[s][i]])
+                DFS(g, g[s][i], used);
+        }
+    }
+
+    public static bool[] BFS(List<List<int>> g, int s)
+    {
+        Queue<int> q = new Queue<int>();
+        bool[] used = new bool[g.Count];
+
+        q.Enqueue(s);
+
+        while(q.Count > 0) 
+        { 
+            var v = q.Dequeue();
+
+            used[v] = true;
+
+            for (int i = 0; i < g[v].Count; i++)
+            {
+                if (!used[g[v][i]])
+                    q.Enqueue(g[v][i]);
+            }
+        }
+
+        return used;
+    }
+
+    public static (bool[], int[]) BFSModified(List<List<int>> g, int s)
+    {
+        Queue<int> q = new Queue<int>();
+        bool[] used = new bool[g.Count];
+        int[] Y = (new int[g.Count]).Select(s => -1).ToArray();
+        Y[s] = 0;
+
+        q.Enqueue(s);
+
+        while (q.Count > 0)
+        {
+            var v = q.Dequeue();
+
+            used[v] = true;
+
+            for (int i = 0; i < g[v].Count; i++)
+            {
+                if (!used[g[v][i]])
+                {
+                    q.Enqueue(g[v][i]);
+                    Y[g[v][i]] = v;
+                }
+            }
+        }
+
+        return (used,Y);
+    }
+}
